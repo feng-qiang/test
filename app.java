2f3a0dac38bdac2bcead3345b30b53b550150894
feng-qiang <61629839+feng-qiang@users.noreply.github.com>
2020-10-22 15:28:16 +0800

    Add files via upload
    第一次上传

---
+++ app.java
@@ -0,0 +1,10 @@
+import master_device.MyFarme;
+
+public class app {
+    public static void main(String[] args) {
+        MyFarme farme = new MyFarme();
+        farme.setVisible(true);
+
+
+    }
+}
